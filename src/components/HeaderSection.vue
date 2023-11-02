4bfc1e93120cf1130c647a9ea8102596464308e8
Alisser <gnlrjswjd@naver.com>
2023-11-02 21:55:54 +0900

    헤더

---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,209 @@
+<template>
+    <header id="header" role="banner">
+        <div class="header__inner">
+            <h1 class="header__logo">
+                <a href="#">portfolio<em>vue.js</em></a>
+            </h1>
+            <nav class="header__nav" role="navigation" aria-label="메인 메뉴">
+                <ul>
+                    <li><a href="#intro">intro</a></li>
+                    <li><a href="#skill">skill</a></li>
+                    <li><a href="#site">site</a></li>
+                    <li><a href="#port">portfolio</a></li>
+                    <li><a href="#contact">contact</a></li>
+                </ul>
+            </nav>
+            <div class="header__nav__mobile" id="headerToggle" aria-controls="primary-menu" aria-expanded="false" role="button" tabindex="0">
+                <span></span>
+            </div>
+        </div>
+    </header>
+</template>
+
+
+<script>
+const headerToggle = document.getElementById("headerToggle");
+const headerNav = document.querySelector(".header__nav");
+
+if (headerToggle) {
+    headerToggle.addEventListener("click", () => {
+    headerNav.classList.toggle("show");
+    headerToggle.getAttribute("aria-expanded") === "true"
+        ? headerToggle.setAttribute("aria-expanded", "false")
+        : headerToggle.setAttribute("aria-expanded", "true");
+    });
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "@/assets/scss/mixin";
+
+
+#header {
+    @include position-fixed;
+    z-index: 10000;
+}
+.header__inner {
+    @include flex-between;
+    background-color: rgba(116, 99, 99, 0.1);
+    backdrop-filter: blur(15px);
+    padding: 1rem;
+
+    .header__logo {
+        font-size: 0.9rem;
+        text-align: center;
+        text-transform: uppercase;
+        line-height: 1;
+
+        em {
+            font-size: 10px;
+            display: block;
+            color: var(--black200);
+        }
+    }
+    
+    .header__nav {
+
+        @media (max-width: 800px){
+            display: none;
+
+            &.show {
+                display: block;
+
+                ul {
+                    display: block;
+                    position: absolute;
+                    right: 0;
+                    top: 68px;
+                    background-color: rgba(116,99,99,0.1);
+                    backdrop-filter: blur(15px);
+                    z-index: 10000;
+                    min-width: 150px;
+                    padding: 20px 0;
+
+                    li {
+                        display: block;
+                        text-align: right;
+
+                        a {
+                            display: inline-block;
+                            padding: 10px;
+                        }
+                    }
+                }
+            }
+            &.show + .header__nav__mobile span::before {
+                width: 20px;
+            }
+            &.show + .header__nav__mobile span::after {
+                width: 20px;
+            }
+        }
+        
+        li {
+            display: inline;
+    
+            a {
+                text-transform: uppercase;
+                font-size: 14px;
+                padding: 14px;
+                position: relative;
+    
+                &::before {
+                    content: '';
+                    width: calc(100% - 28px);
+                    height: 1px;
+                    background-color: var(--black);
+                    position: absolute;
+                    left: 14px;
+                    bottom: 10px;
+                    transform: scaleX(0);
+                    transition: all 0.3s;
+                }
+                &:hover::before {
+                    transform: scaleX(1);
+                }
+            }
+        }
+    }
+    
+    .header__nav__mobile {
+        display: none;
+        width: 40px;
+        height: 40px;
+        cursor: pointer;
+
+        @media (max-width: 800px){
+            display: block;
+        }
+
+        span {
+            display: block;
+            width: 40px;
+            height: 2px;
+            background-color: var(--black);
+            margin-top: 19px;
+            position: relative;
+
+            &::before {
+                content: "";
+                width: 40px;
+                height: 2px;
+                background-color: var(--black);
+                position: absolute;
+                right: 0;
+                top: 6px;
+                transition: width 0.3s;
+            }
+            &::after {
+                content: "";
+                width: 40px;
+                height: 2px;
+                background-color: var(--black);
+                position: absolute;
+                left: 0;
+                bottom: 6px;
+                transition: width 0.3s;
+            }
+        }
+    }
+    
+} 
+@media (max-width: 800px){
+    .header__nav {
+        display: none;
+    }
+    .header__nav.show {
+        display: block;
+    }
+    .header__nav.show ul {
+        display: block;
+        position: absolute;
+        right: 0;
+        top: 68px;
+        background-color: rgba(116,99,99,0.1);
+        backdrop-filter: blur(15px);
+        z-index: 10000;
+        min-width: 150px;
+        padding: 20px 0;
+    }
+    .header__nav.show li {
+        display: block;
+        text-align: right;
+    }
+    .header__nav.show li a {
+        display: inline-block;
+        padding: 10px;
+    }
+    .header__nav.show + .header__nav__mobile span::before {
+        width: 20px;
+    }
+    .header__nav.show + .header__nav__mobile span::after {
+        width: 20px;
+    }
+    .header__nav__mobile {
+        display: block;
+    }
+}
+</style>
