d087b680eb63086c28a438a0c8a3acb72e8a4507
Alisser <gnlrjswjd@naver.com>
2023-11-02 22:46:34 +0900

    인트로 끝

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,3 +20,8 @@ export const headerNav = [
         url: "#contact",
     },
 ];
+
+export const introText = {
+    title: "port developer",
+    desc: ["talent is", "found at the end of the", "effort"],
+};
